e76545827a9359e14f8a5839ef5060d6c340237b
Michael Ballantyne <michael.ballantyne@gmail.com>
2012-02-06 19:05:25 -0700

    format

--- app/_attachments/script/app.js
+++ app/_attachments/script/app.js
@@ -2,35 +2,35 @@
 const maxInt = 9007199254740992;
 randomObject = function(context)
 {
-	callback = function(item)
-	{
-		problems = item['problems'];
-		console.log(problems);
-		randomNum = Math.floor(Math.random() * maxInt) % problems.length;
-		problem = problems[randomNum];
-		this.render('templates/problemTemplate.hb', problem).replace('#displayBox');
-	}
-	context.load('/localhost/problemSet.json').then(callback);
+    callback = function(item)
+    {
+        problems = item['problems'];
+        console.log(problems);
+        randomNum = Math.floor(Math.random() * maxInt) % problems.length;
+        problem = problems[randomNum];
+        this.render('templates/problemTemplate.hb', problem).replace('#displayBox');
+    }
+    context.load('/localhost/problemSet.json').then(callback);
 }
 
 var app = Sammy('#main', function()
 {
-	this.use('Handlebars', 'hb');
-	this.element_selector = '#displayBox';
-	this.get('#/', function()
-	{
-		this.load('/localhost/f89841dd6e9867194d09fe0e1e0026a7',
-		{
-			json : true
-		}).render('templates/someTemplate.hb').swap();
-	});
+    this.use('Handlebars', 'hb');
+    this.element_selector = '#displayBox';
+    this.get('#/', function()
+    {
+        this.load('/localhost/f89841dd6e9867194d09fe0e1e0026a7',
+        {
+            json : true
+        }).render('templates/someTemplate.hb').swap();
+    });
 
-	this.get('#/challenge', function()
-	{
-		randomObject(this);
-	});
+    this.get('#/challenge', function()
+    {
+        randomObject(this);
+    });
 });
 $(function()
 {
-	app.run('#/');
+    app.run('#/');
 });
