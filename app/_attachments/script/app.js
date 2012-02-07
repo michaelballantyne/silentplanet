1ee9bf8e32437748ba739d034ee78251b5271972
Daniel J. "metz" Tanner <djt1216@westminstercollege.edu>
2012-02-06 17:14:38 -0700

    challenge mode stuff

--- app/_attachments/script/app.js
+++ app/_attachments/script/app.js
@@ -1,30 +1,35 @@
 //Apache 2.0 J Chris Anderson 2011
+randomObject = function(context)
+{
+	callback = function(item)
+	{
+		problems = item['problems'];
+		console.log(problems);
+		randomNum = 0;
+		problem = problems[randomNum];
+		this.render('templates/problemTemplate.hb', problem).replace('#displayBox');
+	}
+	context.load('/localhost/problemSet.json').then(callback);
+}
+
 var app = Sammy('#main', function()
-		{
+{
 	this.use('Handlebars', 'hb');
 	this.element_selector = '#displayBox';
 	this.get('#/', function()
-			{
-		this.load('/localhost/f89841dd6e9867194d09fe0e1e0026a7', {json: true}).render('templates/someTemplate.hb').swap();
-			});
-
-//	this.get('#/challenge', function()
-//			{
-//		while (true)
-//		{
-//			problems = this.load('/localhost/problemSet.json');
-//			$('#displayBox').html("<p>" + )
-//		}
-//			})
-//		});
-$(function()
+	{
+		this.load('/localhost/f89841dd6e9867194d09fe0e1e0026a7',
 		{
-	app.run('#/');
-		});
+			json : true
+		}).render('templates/someTemplate.hb').swap();
+	});
 
-//function loadNewRandomProblem()
-//{
-//	problems = this.load('/localhost/problemSet.json');
-//	randomNum = Math.floor(random()) * problems.length;
-//	return 
-//}
+	this.get('#/challenge', function()
+	{
+		randomObject(this);
+	});
+});
+$(function()
+{
+	app.run('#/');
+});
