4475e03d176e7544a36a206e1fd90a1a2f6f869d
D. 'metz' Tanner <djt1216@westminstercollege.edu>
2012-02-11 18:16:28 -0700

    fixed some merging artifacts.

--- app/_attachments/script/app.js
+++ app/_attachments/script/app.js
@@ -47,7 +47,7 @@
 			}
 	};
 
-	var db = $.couch.db('localhost')
+	var db = $.couch.db('localhost');
 
 	var currentProblem;
 
@@ -58,155 +58,166 @@
 		var callback = function(view)
 		{
 			if (view.rows.length == 0)
+			{
 				$('displayBox').html('Empty Database');
-			return;
-		}
-		var randomNum = Math.floor(Math.random() * view.rows.length);
-		var problem = view.rows[randomNum].value;
-		if (problem)
-		{
-			this.render('templates/problemTemplate.hb', problem).appendTo('#displayBox');
-			currentProblem = problem;
-		}
+				return;
+			}
+			var randomNum = Math.floor(Math.random() * view.rows.length);
+			var problem = view.rows[randomNum].value;
+			if (problem)
+			{
+				this.render('templates/problemTemplate.hb', problem).appendTo('#displayBox');
+				currentProblem = problem;
+			}
+		};
+		ProblemSet.getProblems(context, callback);
 	}
-	ProblemSet.getProblems(context, callback);
-		}
 
-var app = Sammy('#main', function()
+	var app = Sammy('#main', function()
+	{
+		this.use('Handlebars', 'hb');
+
+		this.before({except: {path: '#/login'}}, function()
 		{
-	this.use('Handlebars', 'hb');
-
-	this.before({except: {path: '#/login'}}, function()
+			if (username != null)
+				return true;
+			else
 			{
-		if (username != null)
-			return true;
-		else
-		{
-			this.partial('templates/login.hb');
-			return false;
-		}
-			});
-
-	this.get('#/logout', function(){
-		username = null;
-		this.redirect('#/');
-	});
-
-	this.get('#/', function()
-			{
-		this.redirect('#/challenge');
-			});
-
-	this.get('#/challenge', function()
-			{
-		this.partial('templates/game.hb').then(function()
-				{
-			$('#input').focus();
-			randomObject(this);
-				});
-			}); 
-
-	this.post("#/problems", function()
-			{
-		var self = this;
-		ProblemSet.saveProblem(new Problem(this.params['question'], this.params['answer'], this.params['difficulty']), function()
-				{
-			self.redirect('#/problems/new');
-				});
-			});
-
-	this.get("#/problems/new", function()
-			{
-		Handlebars.registerHelper("formatDifficulty", function(difficulty)
-				{
-			var difficultyAsAsterisk = "";
-			for(var i = 0; i < difficulty; i++)
-			{
-				difficultyAsAsterisk += "*";
+				this.partial('templates/login.hb');
+				return false;
 			}
-			return new Handlebars.SafeString(difficultyAsAsterisk);
-				});
-		ProblemSet.getProblems(this, function(view)
-				{
-			this.partial('templates/admin/addproblem.hb', {rows: view.rows})
-			.then(function()
-					{
-				$('#question').focus();
-					});
-				});
-			});
-
-	this.post("#/students", function() {
-		var self = this;
-		StudentSet.saveStudent(new Student(this.params['username']), function() {
-			self.redirect('#/student/new');
 		});
-	});
 
-	this.get("#/students/new", function() {
-		StudentSet.getStudents(this, function(view){
-			this.partial('templates/admin/addstudent.hb', {rows: view.rows})
-			.then(function() {
-				$('#student').focus();
+		this.get('#/logout', function()
+		{
+			username = null;
+			this.redirect('#/');
+		});
+
+		this.get('#/', function()
+		{
+			this.redirect('#/challenge');
+		});
+
+		this.get('#/challenge', function()
+		{
+			this.partial('templates/game.hb').then(function()
+			{
+				$('#input').focus();
+				randomObject(this);
+			});
+		}); 
+
+		this.post("#/problems", function()
+		{
+			var self = this;
+			ProblemSet.saveProblem(new Problem(this.params['question'], this.params['answer'], this.params['difficulty']), function()
+			{
+				self.redirect('#/problems/new');
 			});
 		});
-	});
-	this.post("#/answer", function()
-			{
-		var answer = this.params['answer'];
-		if (answer == currentProblem.answer)
-		{
-			$('#displayBox').append("<br/>");
-			$('#displayBox').append("Correct!");
-			$('#displayBox').append("<br/>");
-			$('#input').val("");
-			randomObject(this);
-		}
-		else
-		{
-			$('#displayBox').append("<br/>");
-			$('#displayBox').append("Incorrect, try again!");
-			$('#displayBox').append("<br/>");
-			$('#input').val("");
-			this.render('templates/problemTemplate.hb', currentProblem).appendTo('#displayBox');
-		}
-			});
 
-	this.post("#/login", function()
+		this.get("#/problems/new", function()
+		{
+			Handlebars.registerHelper("formatDifficulty", function(difficulty)
 			{
-		var context = this;
-		var entered_username = this.params['user'];
-		this.load("/localhost/_design/app/_view/students", {json: true}).then(function(view)
+				var difficultyAsAsterisk = "";
+				for(var i = 0; i < difficulty; i++)
 				{
-			for(var i = 0; i < view.rows.length; i++)
+					difficultyAsAsterisk += "*";
+				}
+				return new Handlebars.SafeString(difficultyAsAsterisk);
+			});
+			
+			ProblemSet.getProblems(this, function(view)
 			{
-				if(view.rows[i].key == entered_username)
-					username = entered_username;
+				this.partial('templates/admin/addproblem.hb', {rows: view.rows})
+				.then(function()
+				{
+					$('#question').focus();
+				});
+			});
+		});
+
+		this.post("#/students", function()
+		{
+			var self = this;
+			StudentSet.saveStudent(new Student(this.params['username']), function()
+			{
+				self.redirect('#/students/new');
+			});
+		});
+
+		this.get("#/students/new", function()
+		{
+			StudentSet.getStudents(this, function(view)
+			{
+				this.partial('templates/admin/addstudent.hb', {rows: view.rows})
+				.then(function()
+				{
+					$('#student').focus();
+				});
+			});
+		});
+		
+		this.post("#/answer", function()
+		{
+			var answer = this.params['answer'];
+			if (answer.toUpperCase() == currentProblem.answer.toUpperCase())
+			{
+				$('#displayBox').append("<br/>");
+				$('#displayBox').append("Correct!");
+				$('#displayBox').append("<br/>");
+				$('#input').val("");
+				randomObject(this);
 			}
-
-			context.redirect('#/')
-				});
-			});
-
-	this.get("#/problems/delete/:id/:rev", function()
+			else
 			{
-		var self = this;
-		ProblemSet.deleteProblem(this.params['id'], this.params['rev'], function()
-				{
-			self.redirect("#/problems/new");
-				});
-			});
+				$('#displayBox').append("<br/>");
+				$('#displayBox').append("Incorrect, try again!");
+				$('#displayBox').append("<br/>");
+				$('#input').val("");
+				this.render('templates/problemTemplate.hb', currentProblem).appendTo('#displayBox');
+			}
+		});
 
-	this.get("#/students/delete/:id/:rev", function() {
-		var self = this;
-		StudentSet.deleteStudent(this.params['id'], this.params['rev'], function() {
-			self.redirect("#/students/new");
+		this.post("#/login", function()
+		{
+			var context = this;
+			var entered_username = this.params['user'];
+			this.load("/localhost/_design/app/_view/students", {json: true}).then(function(view)
+			{
+				for(var i = 0; i < view.rows.length; i++)
+				{
+					if(view.rows[i].key == entered_username)
+						username = entered_username;
+				}
+
+				context.redirect('#/')
+			});
+		});
+
+		this.get("#/problems/delete/:id/:rev", function()
+		{
+			var self = this;
+			ProblemSet.deleteProblem(this.params['id'], this.params['rev'], function()
+			{
+				self.redirect("#/problems/new");
+			});
+		});
+
+		this.get("#/students/delete/:id/:rev", function()
+		{
+			var self = this;
+			StudentSet.deleteStudent(this.params['id'], this.params['rev'], function()
+			{
+				self.redirect("#/students/new");
+			});
 		});
 	});
-		});
 
-$(function()
-		{
-	app.run('#/');
-		});
+	$(function()
+	{
+		app.run('#/');
+	});
 })(jQuery);
