5c38e6b023cb19b415c946d81c564e1ba18ef159
Matthew Castillo <mwernerwolfgang@gmail.com>
2012-03-25 22:59:11 -0600

    merge

--- app/_attachments/scripts/controllers/challenge.js
+++ app/_attachments/scripts/controllers/challenge.js
@@ -1,9 +1,38 @@
 define(['libraries/jquery', 'libraries/sammy', 'models/problems', 'models/problemreports', 'controllers/login'], function ($, sammy, problemSet, problemReports, login) {
+    var currentProblem = null,
+        challenge = {};
+        
+    challenge.chooseRandomProblem = function (problems, student) {
+        var randomNum, problem;
+        do {
+            randomNum = Math.floor(Math.random() * problems.length);
+            problem = problems[randomNum].value;
+        }
+        while(problem.difficulty > student.difficultySetting);
+        return problem;
+    };
+    
+    challenge.randomObject = function (context) {
+        var callback = function (view) {
+            if (view.rows.length === 0) {
+                $('displayBox').html('Empty Database');
+                return;
+            }
+            
+            var problem = challenge.chooseRandomProblem(view.rows, login.currentStudent);
+            
+            if (problem) {
+                this.render('templates/problem.hb', problem).appendTo('#displayBox');
+                currentProblem = problem;
+            }
+        };
+        problemSet.getProblems(context, callback);
+    };
     sammy('#main', function () {
         this.get('#/challenge', function () {
             this.partial('templates/game.hb').then(function () {
                 $('#input').focus();
-                problemSet.pullRandomProblem(this);
+                randomObject(this);
             });
         });
 
@@ -19,12 +48,13 @@ define(['libraries/jquery', 'libraries/sammy', 'models/problems', 'models/proble
                     $('#displayBox').append("<br/>");
                     $('#input').val("");
                     problemSet.pullRandomProblem(this);
+                    randomObject(this);
                 } else {
                     $('#displayBox').append("<br/>");
                     $('#displayBox').append("Incorrect, try again!");
                     $('#displayBox').append("<br/>");
                     $('#input').val("");
-                    this.render('templates/problem.hb', login.currentProblem).appendTo('#displayBox');
+                    this.render('templates/problem.hb', currentProblem).appendTo('#displayBox');
                 }
             }
             else {
@@ -32,4 +62,6 @@ define(['libraries/jquery', 'libraries/sammy', 'models/problems', 'models/proble
             }
         });
     });
+    
+    return challenge;
 });
