75582ee68286127e0e216e120503774216830c4b
Michael Ballantyne <michael.ballantyne@gmail.com>
2012-04-16 19:34:39 -0600

    typing text change

--- app/_attachments/scripts/controllers/movecommands.js
+++ app/_attachments/scripts/controllers/movecommands.js
@@ -9,10 +9,10 @@ define(['libraries/jquery', 'models/items', 'controllers/login', 'models/rooms',
     visited = null,
 
     //assumes there is no roomFlag for the current room, adds just the simple one
-    visit = function (context, cont) {
+    visit = function (context) {
         roomSet.addOrUpdateRoomFlag(roomLogic.currentRoom._id, roomLogic.currentRoom._id);
         login.updateStudentOnServer();
-        look.look(["look"], roomLogic.currentRoom, context, cont);
+        look.look(["look"], roomLogic.currentRoom, context);
     },
 
     //checks to see if the room is in an updated state and returns the most recent state
@@ -28,9 +28,8 @@ define(['libraries/jquery', 'models/items', 'controllers/login', 'models/rooms',
     },
 
     //displays the basic error message if a command was not parseable for some reason
-    errorMessage = function (cont) {
+    errorMessage = function () {
         display.append("I'm sorry, I didn't understand that.  Can you try saying it a different way?");
-        cont();
     };
 
     moveCommands.findPlayerRoom = function () {
@@ -48,25 +47,25 @@ define(['libraries/jquery', 'models/items', 'controllers/login', 'models/rooms',
     };
 
     //needed to separate out for initial move
-    moveCommands.moveTo = function (roomID, context, cont) {
+    moveCommands.moveTo = function (roomID, context) {
         roomID = getUpdatedRoomState(roomID);
         while (roomID !== getUpdatedRoomState(roomID)) {
             roomID = getUpdatedRoomState(roomID);
         }
         roomSet.getRoom(roomID, context, function (view) {
             if (view.rows.length !== 1) {
-                errorMessage(cont);
+                errorMessage();
             } else {
                 var roomVals = view.rows[0].value;
                 roomLogic.currentRoom = roomSet.createRoom(roomVals._id, roomVals.name, roomVals.description, roomVals.exits, roomVals.items, roomVals.problemDescription, roomVals.problemWrapUp, roomVals.nextState);
                 items.moveItem(items.PLAYER_MARKER, roomLogic.currentRoom._id);
                 
                 var activateCallback = function () {
-                    probLogic.activateProblem(roomLogic.currentRoom.problemDescription, context, cont);
+                    probLogic.activateProblem(roomLogic.currentRoom.problemDescription, context);
                 }
                 
                 if (!visited) {
-                    visit(context, cont);
+                    visit(context);
                 } else {
                     display.append(roomLogic.currentRoom.name);
                     itemLogic.displayItems(context, activateCallback);
@@ -76,13 +75,12 @@ define(['libraries/jquery', 'models/items', 'controllers/login', 'models/rooms',
     };
 
     //basic move command.  Attempts to move in the direction given
-    moveCommands.move = function (direction, context, cont) {
+    moveCommands.move = function (direction, context) {
         var directionDialog = roomLogic.currentRoom.getDirection(direction);
         if (!directionDialog.roomID) {
             display.append(directionDialog.description);
-            cont();
         } else {
-            this.moveTo(directionDialog.roomID, context, cont);
+            this.moveTo(directionDialog.roomID, context);
         }
     };
     return moveCommands;
